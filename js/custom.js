672fdc99eb09ceb49e0ef7c1d0e312d646da3e29
Little Dent <littledentgopo@gmail.com>
2017-09-16 15:03:37 +0300

    2 team

--- js/custom.js
+++ js/custom.js
@@ -29,9 +29,9 @@
  
         autoPlay: 5000, //Set AutoPlay to 5 seconds
 
-        items : 4,
-        itemsDesktopSmall : [979,3],
-        stopOnHover: true
+        items : 2,
+        itemsDesktopSmall : [979,2],
+        stopOnHover: true,
  
     });
 
